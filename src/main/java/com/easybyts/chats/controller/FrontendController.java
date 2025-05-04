6e2cda4fa38f0128e88d7a9bf550e5ad605554db
Kunal Dangi <kunaldangi252@gmail.com>
2025-05-04 23:20:08 +0530

    feat: added redirection if not valid cookie

--- src/main/java/com/easybyts/chats/controller/FrontendController.java
+++ src/main/java/com/easybyts/chats/controller/FrontendController.java
@@ -1,13 +1,63 @@
 package com.easybyts.chats.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import com.easybyts.chats.util.JwtUtil;
 
 @Controller
 public class FrontendController {
+
+    private final JwtUtil jwtUtil;
+
+    public FrontendController(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @GetMapping("/")
+    public String handleRoot(HttpServletRequest request, HttpServletResponse response) {
+        String token = getJwtFromCookies(request);
+
+        if (token == null) {
+            return "redirect:/login";
+        }
+
+        try {
+            String data = jwtUtil.validateTokenAndGetSubject(token);
+
+            Map<String, Object> userData = new ObjectMapper().readValue(data, new TypeReference<Map<String, Object>>() {});
+            request.setAttribute("user", userData); // get the data using userData.get("id")
+
+            return "forward:/index.html";
+        } catch (Exception e) {
+            return "redirect:/login";
+        }
+    }
     
     @RequestMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
         return "forward:/index.html";
+    }
+
+
+    private String getJwtFromCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) return null;
+
+        for (Cookie cookie : cookies) {
+            if ("session".equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return null;
     }
 }
